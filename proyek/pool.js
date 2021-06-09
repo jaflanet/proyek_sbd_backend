680ec5441e328dd9a12572264e1c8e10d26ebfc4
aidan <muhammad.aidan@ui.ac.id>
2021-06-09 17:05:36 +0700

    aku update

--- proyek/pool.js
+++ proyek/pool.js
@@ -6,7 +6,7 @@ const databaseConfig = new Pool({
     user: "hmthsyfvkvmlxo",
     host: "ec2-18-214-140-149.compute-1.amazonaws.com",
     database: "dd39j3vf9eql3",
-    password: "c0a2b297252908007bc202555c09852bf5fe44e06bbd4b24460c0a0ee565458c",
+    password:process.env.PASS,
     port: "5432",
     ssl: {
         rejectUnauthorized: false
